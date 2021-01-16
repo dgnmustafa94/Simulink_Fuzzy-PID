d141f130220c735af5ae9c4495030a73ab2a556a
Mustafa Doğan <39681638+doganmustafa1994@users.noreply.github.com>
2021-01-17 00:30:23 +0300

    Add files via upload

---
+++ Start.m
@@ -0,0 +1,21 @@
+clear;
+
+% % Motor parameter % % 
+M = 0.5;
+m = 0.2;
+b = 0.1;
+I = 0.006;
+g = 9.81;
+L = 0.6;
+pi=pi;
+
+% % First alternative % % 
+if(gcs~="firstAlternative.slx")
+    firstAlternative;
+end
+
+% % Second alternative % % 
+
+% if(gcs~="secondAlternative.slx")
+%     secondAlternative.slx;
+% end
